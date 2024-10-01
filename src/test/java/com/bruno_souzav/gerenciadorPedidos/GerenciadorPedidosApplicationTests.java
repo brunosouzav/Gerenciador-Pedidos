d0cda2ab2223065d85ae8e0b7630392d63c31501
Bruno Souza <brunocloud78@gmail.com>
2024-10-01 13:51:18 -0300

    Gerenciador de Pedidos

---
+++ src/test/java/com/bruno_souzav/gerenciadorPedidos/GerenciadorPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno_souzav.gerenciadorPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
